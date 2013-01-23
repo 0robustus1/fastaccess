69772d791fc936c09be050d4546269b1f7b5ceff
robustus <robustus@rightsrestricted.com>
2013-01-23 22:03:05 +0100

    implementing test for multiple call on same class
    also changed title of first test to
    something more concise.

--- test/dummy/app/models/simple_string.rb
+++ test/dummy/app/models/simple_string.rb
@@ -1,8 +1,13 @@
 class SimpleString < ActiveRecord::Base
   # attr_accessible :title, :body
   acts_with_fastaccess_on :simple_string
+  acts_with_fastaccess_on :other_string
 
   def simple_string
     "this is a simple string"
+  end
+
+  def other_string
+    "this is another string"
   end
 end
--- test/fastaccess_test.rb
+++ test/fastaccess_test.rb
@@ -5,8 +5,16 @@ class FastaccessTest < ActiveSupport::TestCase
     assert_kind_of Module, Fastaccess
   end
 
-  def test_hickwalls_squawk_should_populate_last_squawk
+  def test_simple_redis_write_and_read
     string = SimpleString.create
     assert_equal string.simple_string, $redis.get("simple_string_#{string.class}-#{string.id}")
   end
+
+  def test_redis_write_and_read_on_parallel_methods
+    string = SimpleString.create
+    assert_equal string.simple_string, $redis.get("simple_string_#{string.class}-#{string.id}")
+    assert_equal string.other_string, $redis.get("other_string_#{string.class}-#{string.id}")
+    assert_not_equal string.simple_string, string.other_string
+  end
+
 end
